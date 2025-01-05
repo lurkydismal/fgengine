69a2208ac36c07c07c9630052697f9083fe0d82e
lurkydismal <lurkydismal@duck.com>
2025-01-05 02:09:35 +0000

    Add boxes_t to state_t as a property and flag ro render fill boxes

--- state_t/include/state_t.h
+++ state_t/include/state_t.h
@@ -4,13 +4,18 @@
 #include <SDL3/SDL_video.h>
 
 #include "animation_t.h"
+#include "boxes_t.h"
 
-#define DEFAULT_STATE \
-    { .animation = DEFAULT_ANIMATION, .isActionable = true, .renderer = NULL }
+#define DEFAULT_STATE                 \
+    { .renderer = NULL,               \
+      .animation = DEFAULT_ANIMATION, \
+      .boxes = DEFAULT_BOXES,         \
+      .isActionable = true }
 
 typedef struct {
     SDL_Renderer* renderer;
     animation_t animation;
+    boxes_t boxes;
     bool isActionable;
 } state_t;
 
@@ -20,4 +25,6 @@ state_t state_t$create( SDL_Renderer* _renderer,
                         bool _isActionable );
 void state_t$destroy( state_t* _state );
 void state_t$step( state_t* _state, bool _canLoop );
-void state_t$render( state_t* _state, SDL_FRect* _targetRectanble );
+void state_t$render( state_t* _state,
+                     const SDL_FRect* _targetRectanble,
+                     bool _doDrawBoxes );
--- state_t/src/state_t.c
+++ state_t/src/state_t.c
@@ -27,18 +27,38 @@ state_t state_t$create( SDL_Renderer* _renderer,
         SDL_free( l_pattern );
     }
 
+    // Load boxes
+    {
+        char* l_pattern = duplicateString( "_*." );
+
+        // _name.txt
+        concatBeforeAndAfterString( &l_pattern, _name, "txt" );
+
+        l_returnValue.boxes = boxes_t$load( _path, l_pattern );
+
+        SDL_free( l_pattern );
+    }
+
     return ( l_returnValue );
 }
 
 void state_t$destroy( state_t* _state ) {
     animation_t$unload( &( _state->animation ) );
+    boxes_t$unload( &( _state->boxes ) );
 }
 
 void state_t$step( state_t* _state, bool _canLoop ) {
     animation_t$step( &( _state->animation ), _canLoop );
+    boxes_t$step( &( _state->boxes ), _canLoop );
 }
 
-void state_t$render( state_t* _state, SDL_FRect* _targetRectanble ) {
+void state_t$render( state_t* _state,
+                     const SDL_FRect* _targetRectanble,
+                     bool _doDrawBoxes ) {
     animation_t$render( _state->renderer, &( _state->animation ),
                         _targetRectanble );
+    if ( _doDrawBoxes ) {
+        boxes_t$render( _state->renderer, &( _state->boxes ), _targetRectanble,
+                        true );
+    }
 }
